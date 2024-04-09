0c060e37bb80f5823fafc03e3ba01c0c85b616af
LearningToCode-js <79099808+LearningToCode-js@users.noreply.github.com>
2024-04-09 12:17:06 -0400

    Started narrative

--- src/Tester.java
+++ src/Tester.java
@@ -6,16 +6,32 @@ import java.util.ArrayList;
 public class Tester {
     static ArrayList<Musician> bus = new ArrayList<>();
     public static void main(String[] args) {
-        decideContestants();
+        introduceNarrative();
+        decideContestantsPlayer();
+        decideContestantsComposer();
+        decideContestantsSinger();
+        decideContestantsBand();
         boardTheBus();
         onTheBusCompetition();
         decideWhoProceeds();
         departure();
     }
 
+    public static void introduceNarrative() {
+        System.out.println("Once upon a time in a land far far away...");
+        System.out.println("There were several neighborhoods in which the musicians live: the player, composer, singer, and band neighborhoods.");
+        System.out.println("Every year, a bus comes to each of the neighborhoods to take two musicians to compete in a grand tournament to determine who the best musician is in all neighbourhoods.");
+        System.out.println("To decide who will go on the bus, the musicians from each neighbourhood will vote for two musicians that will progress to the grand bus.");
+        System.out.println("Although a total of eight musicians will make it onto the bus, only five can exit, to be determined by a performance and subsequent vote on the bus.");
+        System.out.println("Once the 5 remaining musicians get off the bus, they will undergo a final tournament watched by all people to decide the winner.");
+        System.out.println();
+        System.out.println();
+        System.out.println("This is the story of one strange year, where the bus took turns that nobody expected...");
+    }
 
+    public static void decideContestantsPlayer() {
 
-    public static void decideContestants() {
+        Musician mrHansen = new Player("Mr. Hansen", "Piano", false);
         // Instantiating musicians with polymorphism.
         Musician yoYoMa = new Player("Yo-Yo Ma", "Cello", true);
         Musician louisArmstrong = new Player("Louis Armstrong", "Trumpet", true);
@@ -37,6 +53,17 @@ public class Tester {
         bus.add(u2);
 
         System.out.println("Contestants from each neighborhood have been decided and added to the bus.");
+    }
+
+    public static void decideContestantsBand() {
+
+    }
+    public static void decideContestantsSinger() {
+
+    }
+
+    public static void decideContestantsComposer() {
+
     }
 
     public static void boardTheBus() {
